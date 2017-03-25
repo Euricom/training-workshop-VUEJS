945a6136c13654dcb349c48452143d3a5d11c67f
Robbert Wolfs <robbertwolfs@gmail.com>
2017-03-25 15:25:31 +0100

    Setup advanced example

---
+++ examples/advanced/app.js
@@ -0,0 +1,21 @@
+Vue.component('markdown', {
+  props: ['markdown'],
+  template: '<div v-html="compiledMarkdown"></div>',
+  computed: {
+        compiledMarkdown: function () {
+            return marked(this.markdown, { sanitize: true })
+        }
+    },
+})
+
+new Vue({
+    el: '#app',
+    data: {
+        input: '# Advanced exercise example\n\n## Markdown editor',
+    },
+    methods: {
+        update: function (e) {
+            this.input = e.target.value
+        }
+    }
+})
